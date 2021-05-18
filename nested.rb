3025bbc3467f73807eab492cddcb65f374d39fcb
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-05-17 23:29:18 -0700

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,6 +83,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+
+    programmer_hash
 
 end
 
@@ -103,6 +111,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -124,6 +134,10 @@ def adding_to_dennis
         :known_for => "Unix",
         :languages => ["C"]
       }
+
+     
     }
+     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+     programmer_hash
 
 end
